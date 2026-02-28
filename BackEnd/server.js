50885da63f75ecb0ed836f89e49af04d80c429e8
ayushrayal19@gmail.com <ayushrayal19@gmail.com>
2026-02-28 18:45:30 +0530

    Restart The whole project

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,9 +1,6 @@
-require("dotenv").config()
-const app =require("./src/app");
+const app = require("./src/app");
 const port = 3000;
 
-
-
 app.listen(port,()=>{
-    console.log("Server is running on http://localhost:3000");
-})
+    console.log("Server runs http://localhost:3000");
+});
--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,9 +1,4 @@
-const express = require('express');
-
-
+const express = require("express");
 const app = express();
-app.use(express.json());
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
-module.exports = app
+
+module.exports = app;
